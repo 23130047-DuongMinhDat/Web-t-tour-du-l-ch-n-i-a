65af4f0b6954437595ce18a9bf8f3e85b83d9fc0
Vo Tan Sang <22130233@st.hcmuaf.edu.vn>
2025-11-21 19:11:46 +0700

    refactor: thêm dữ liệu mẫu, sửa style bookings, tour admin

--- html/assets/admin/js/bookings.js
+++ html/assets/admin/js/bookings.js
@@ -1,13 +1,3 @@
 $('#bookingsTable').DataTable({
     ...dataTableConfig,
-    columns: [
-        // ... các columns khác
-        {
-            data: 'status',
-            render: function (data) {
-                return createBookingStatusBadge(data);
-            }
-        },
-        // ... columns khác
-    ]
 });
